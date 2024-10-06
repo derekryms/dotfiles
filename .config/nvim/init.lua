d7e3ae1acb755cfbf79a8a4a89c11277e45f0c54
Derek Ryms <derekryms@gmail.com>
2024-10-06 16:58:09 -0400

    Add search casing options

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,5 +2,8 @@ require("config.lazy")
 
 vim.cmd[[colorscheme tokyonight-night]]
 
-vim.o.number=true
-vim.o.relativenumber=true
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
