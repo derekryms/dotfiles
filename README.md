3d4c97fba7b481dbb66bec3f332d793871036623
Derek Ryms <derekryms@gmail.com>
2024-10-13 02:59:18 -0400

    Make an install script that can run everything in the correct order, and add wezterm.lua

--- README.md
+++ README.md
@@ -2,43 +2,12 @@
 
 This directory contains the dotfiles for my system
 
-## Requirements
-
-Ensure you have the following installed on your system
-
-### Homebrew
-
-```
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-```
-
-Ensure to add the following to the .zprofile
-
-```
-eval "$(/opt/homebrew/bin/brew shellenv)"
-```
-
-### Brew Installs
-
-```
-brew install stow
-brew install --cask alacritty
-brew install neovim
-brew install ripgrep
-brew install fd
-```
-
 ## Installation
 
-First, check out the dotfiles repo in your $HOME directory using git
+Download the install.zsh and run the following commands
 
 ```
-$ git clone git@github.com/derekryms/dotfiles.git
-$ cd dotfiles
-```
-
-then use GNU stow to create symlinks
-
-```
-$ stow .
+chmod +x install.zsh
+./install.zsh
+source ~/.zprofile
 ```
---
+++ install.zsh
@@ -0,0 +1,52 @@
+#!/bin/zsh
+
+homebrewEval='eval "$(/opt/homebrew/bin/brew shellenv)"'
+echo "Installing homebrew"
+if ! command -v brew &> /dev/null; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	echo -e "\nAdding homebrew to PATH"
+	if ! grep -Fxq "$homebrewEval" ~/.zprofile; then
+		echo "$homebrewEval" >> ~/.zprofile
+		echo -e "\nSourcing .zprofile in this session"
+		source ~/.zprofile
+	else
+		echo "Homebrew already added to PATH"
+	fi
+else
+	echo "Homebrew is already installed"
+	echo "Updating homebrew"
+	brew update
+fi
+
+echo -e "\nInstall stow"
+brew install stow
+
+echo -e "\nInstall neovim"
+brew install neovim
+
+echo -e "\nInstall ripgrep"
+brew install ripgrep
+
+echo -e "\nInstall fd"
+brew install fd
+
+echo -e "\nInstall alacritty"
+brew install --cask alacritty
+
+echo -e "\nInstall wezterm"
+brew install --cask wezterm
+
+dotfilesPath=~/dotfiles
+echo -e "\nClone dotfiles repo"
+if [ -d "$dotfilesPath" ]; then
+	echo "Dotfiles files folder already exist"
+	echo "Pulling latest changes"
+	git checkout main
+	git pull
+else
+	git clone https://github.com/derekryms/dotfiles.git "$dotfilesPath"
+fi
+
+echo -e "\nRun stow to create symlinks"
+cd ~/dotfiles
+stow .
