2188e7c26fa123412f0c14376e0e0fd7f41fafe1
Derek Ryms <dryms@arielcorp.com>
2024-10-16 13:17:37 -0400

    Add some more to wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require 'wezterm'
-
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 config.color_scheme = 'tokyonight_night'
 config.font = wezterm.font 'JetBrains Mono'
@@ -9,10 +9,27 @@ config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.window_background_opacity = 0.9
 config.window_padding = {
-  left = 5,
-  right = 5,
-  top = 5,
-  bottom = 5,
+	left = 5,
+	right = 5,
+	top = 5,
+	bottom = 5,
+}
+
+config.keys = {
+	{
+		key = 'W',
+		mods = 'CMD',
+		action = act.CloseCurrentTab { confirm = false },
+	},
+	{
+		key = 'K',
+		mods = 'CTRL|SHIFT',
+		action = act.Multiple {
+			act.ClearScrollback 'ScrollbackAndViewport',
+			act.SendKey { key = 'L', mods = 'CTRL' },
+			act.SendKey { key = 'U', mods = 'CTRL' },
+		},
+	}
 }
 
 return config
