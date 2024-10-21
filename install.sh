1d0fb98bcf752228ea20ee6831075dea2a6cc28b
Derek Ryms <dryms@arielcorp.com>
2024-10-21 11:12:28 -0400

    Use bash script and add print utils to start over

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+source scripts/printUtils.sh
+
+info "Installing Homebrew..."
+if hash brew &>/dev/null; then
+	warning "Homebrew already installed"
+else
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+info "Installing Neovim..."
+brew install neovim
