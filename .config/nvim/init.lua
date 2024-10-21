db01901c4df2f1ab53dc50a9efa09508f2406b63
Derek Ryms <dryms@arielcorp.com>
2024-10-21 14:59:31 -0400

    Move weztem config, remove scripts directory, add options and keymaps files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,5 @@
 require("config.lazy")
 
-vim.o.number = true
-vim.o.relativenumber = true
-
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
