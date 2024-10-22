baec1b6906be984f5c7722fb4f395132211bd504
Derek Ryms <dryms@arielcorp.com>
2024-10-22 11:21:36 -0400

    Wezterm config got formatted

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,11 +1,11 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 local act = wezterm.action
 
 config = {
 	automatically_reload_config = true,
-	color_scheme = 'tokyonight_night',
-	font = wezterm.font("JetBrainsMono Nerd Font", {weight="Regular", stretch="Normal", style="Normal"}),
+	color_scheme = "tokyonight_storm",
+	font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" }),
 	font_size = 16.0,
 	use_fancy_tab_bar = false,
 	tab_bar_at_bottom = true,
@@ -21,21 +21,20 @@ config = {
 	default_cursor_style = "BlinkingBar",
 	keys = {
 		{
-			key = 'W',
-			mods = 'CMD',
-			action = act.CloseCurrentTab { confirm = false },
+			key = "W",
+			mods = "CMD",
+			action = act.CloseCurrentTab({ confirm = false }),
 		},
 		{
-			key = 'K',
-			mods = 'CTRL|SHIFT',
-			action = act.Multiple {
-				act.ClearScrollback 'ScrollbackAndViewport',
-				act.SendKey { key = 'L', mods = 'CTRL' },
-				act.SendKey { key = 'U', mods = 'CTRL' },
-			},
-
-		}
-	}
+			key = "K",
+			mods = "CTRL|SHIFT",
+			action = act.Multiple({
+				act.ClearScrollback("ScrollbackAndViewport"),
+				act.SendKey({ key = "L", mods = "CTRL" }),
+				act.SendKey({ key = "U", mods = "CTRL" }),
+			}),
+		},
+	},
 }
 
 return config
