bb9aa882d6c7b328dea4b4f4a22b26bd598d04c0
Derek Ryms <dryms@arielcorp.com>
2024-10-25 15:05:34 -0400

    Show total lines in columns in status line

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -879,10 +879,10 @@ require('lazy').setup({
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
       -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
+      -- ---@diagnostic disable-next-line: duplicate-set-field
+      -- statusline.section_location = function()
+      --   return '%2l:%-2v'
+      -- end
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
