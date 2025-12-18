e20ff367a23b43b87cb57aea7414c12ca1d32921
Derek Ryms <derekryms@gmail.com>
2025-12-18 15:46:10 -0500

    Add basic options, add keymaps file, and move oil keymap to plugin

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,17 @@
 return {
-	'stevearc/oil.nvim',
-	opts = {},
-	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-	lazy = false,
+    'stevearc/oil.nvim',
+    opts = {
+        view_options = {
+            show_hidden = true,
+        },
+        float = {
+            max_width = 0.75,
+            max_height = 0.75,
+        }
+    },
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+    lazy = false,
+    keys = {
+        { "<leader>e", "<CMD>Oil --float<CR>", desc = "Open oil in float" },
+    },
 }
