0ae2def71a3fd0f0b0b2fb3bfa76dc8c51361d7f
Derek Ryms <derekryms@gmail.com>
2025-12-18 15:48:45 -0500

    Add some more basic keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,11 @@
 vim.keymap.set("n", "<ESC>", "<CMD>nohlsearch<CR>", { desc = "Remove search highlighting" })
+
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Up and center" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Down and center" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Search next and center" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Search previous and center" })
+
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Buffer above" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Buffer below" })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Buffer left" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Buffer right" })
