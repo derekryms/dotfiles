9c7388e6fe1be4fbce43b703168afb42321233ad
Derek Ryms <derekryms@gmail.com>
2026-03-21 14:28:21 -0400

    Use side by side in lazygit with delta, update brew packages, and format

--- install.sh
+++ install.sh
@@ -4,40 +4,50 @@ echo
 
 # Check if curl is installed
 if ! command -v curl >/dev/null 2>&1; then
-    echo -e "Please install curl then run the install script again.\n"
-    exit 1
+  echo -e "Please install curl then run the install script again.\n"
+  exit 1
 fi
 
 # Look for and source nvm for script session
 if [ -s "$NVM_DIR/nvm.sh" ]; then
-    \. "$NVM_DIR/nvm.sh"
+  \. "$NVM_DIR/nvm.sh"
 fi
 
 # Install nvm if not already installed
 if ! command -v nvm >/dev/null 2>&1; then
-    echo -e "Installing nvm v0.40.3 ...\n"
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+  echo -e "Installing nvm v0.40.3 ...\n"
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 fi
 
 # Install homebrew if not already installed
 if ! command -v brew >/dev/null 2>&1; then
-    echo -e "Installing homebrew ...\n"
-    /usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo -e "Installing homebrew ...\n"
+  /usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Install/Update homebrew packages
 packages=(
-  "stow"
-  "oh-my-posh"
-  "fzf"
-  "tree-sitter-cli"
-  "wget"
-  "fd"
-  "ripgrep"
-  "neovim"
-  "lazygit"
-  "zsh-autosuggestions"
-  "zsh-syntax-highlighting"
+  # "azure-cli",
+  # "azure-functions-core-tools@4",
+  "fd",
+  "fzf",
+  # "gh",
+  "git-delta",
+  # "imagemagick",
+  "lazygit",
+  "neovim",
+  "oh-my-posh",
+  "ripgrep",
+  # "ruby",
+  # "socat",
+  "stow",
+  # "tmux",
+  "tree",
+  "tree-sitter-cli",
+  # "watchman",
+  "wget",
+  "zsh-autosuggestions",
+  "zsh-syntax-highlighting",
 )
 
 for pkg in "${packages[@]}"; do
@@ -47,8 +57,8 @@ done
 
 # Install claude code if not already installed
 if ! command -v brew >/dev/null 2>&1; then
-    echo -e "Installing claude code ...\n"
-    curl -fsSL https://claude.ai/install.sh | bash
+  echo -e "Installing claude code ...\n"
+  curl -fsSL https://claude.ai/install.sh | bash
 fi
 
 echo -e "Install Script Ran Successfully!\n"
