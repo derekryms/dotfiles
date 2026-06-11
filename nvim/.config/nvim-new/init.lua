5af813c183fa0483bf30060526f987c3c52e7603
Derek Ryms <derekryms@gmail.com>
2026-06-11 15:54:58 -0400

    Format and add hl yank autocmd

--- nvim/.config/nvim-new/init.lua
+++ nvim/.config/nvim-new/init.lua
@@ -33,10 +33,10 @@ vim.o.wrap = false -- disable wrapping
 vim.o.breakindent = true -- prevent line wrapping
 
 vim.pack.add({
-	'https://github.com/mason-org/mason.nvim',
-	'https://github.com/neovim/nvim-lspconfig',
-	'https://github.com/folke/lazydev.nvim',
-	'https://github.com/stevearc/oil.nvim'
+  'https://github.com/mason-org/mason.nvim',
+  'https://github.com/neovim/nvim-lspconfig',
+  'https://github.com/folke/lazydev.nvim',
+  'https://github.com/stevearc/oil.nvim'
 })
 
 require('mason').setup()
@@ -46,22 +46,27 @@ vim.lsp.enable({ 'lua_ls' })
 vim.keymap.set('n', 'gl', vim.diagnostic.open_float)
 
 require('lazydev').setup({
-	library = {
-		-- See the configuration section for more details
-		-- Load luvit types when the `vim.uv` word is found
-		{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-	},
+  library = {
+    -- See the configuration section for more details
+    -- Load luvit types when the `vim.uv` word is found
+    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+  },
 })
 
 require("oil").setup({
-	view_options = {
-		show_hidden = true,
-	},
-	float = {
-		max_width = 0.75,
-		max_height = 0.75,
-		preview_split = "below",
-	},
+  view_options = {
+    show_hidden = true,
+  },
+  float = {
+    max_width = 0.75,
+    max_height = 0.75,
+    preview_split = "below",
+  },
 })
 vim.keymap.set('n', '<leader>e', '<cmd>Oil --float<cr>')
 
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('YankHighlight', { clear = true }),
+  callback = function() vim.hl.on_yank() end,
+})
