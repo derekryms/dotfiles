ceba4aff3127b848bdad44355c1efef50c8d0bad
Derek Ryms <derekryms@gmail.com>
2026-06-11 14:47:39 -0400

    Add oil.nvim

--- nvim/.config/nvim-new/init.lua
+++ nvim/.config/nvim-new/init.lua
@@ -1,21 +1,5 @@
-vim.pack.add({
-	'https://github.com/mason-org/mason.nvim',
-	'https://github.com/neovim/nvim-lspconfig',
-	'https://github.com/folke/lazydev.nvim'
-})
-
-require('mason').setup()
-require('lazydev').setup({
-	library = {
-		-- See the configuration section for more details
-		-- Load luvit types when the `vim.uv` word is found
-		{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-	},
-})
-
-vim.lsp.enable({ 'lua_ls' })
-
-vim.keymap.set('n', 'gl', vim.diagnostic.open_float)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 vim.o.autocomplete = true
 vim.opt.complete:append('o')
@@ -25,3 +9,37 @@ vim.o.pumborder = 'rounded'
 
 vim.o.number = false
 vim.o.relativenumber = true
+
+vim.pack.add({
+	'https://github.com/mason-org/mason.nvim',
+	'https://github.com/neovim/nvim-lspconfig',
+	'https://github.com/folke/lazydev.nvim',
+	'https://github.com/stevearc/oil.nvim'
+})
+
+require('mason').setup()
+
+-- [[ LSP ]]
+vim.lsp.enable({ 'lua_ls' })
+vim.keymap.set('n', 'gl', vim.diagnostic.open_float)
+
+require('lazydev').setup({
+	library = {
+		-- See the configuration section for more details
+		-- Load luvit types when the `vim.uv` word is found
+		{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+	},
+})
+
+require("oil").setup({
+	view_options = {
+		show_hidden = true,
+	},
+	float = {
+		max_width = 0.75,
+		max_height = 0.75,
+		preview_split = "below",
+	},
+})
+vim.keymap.set('n', '<leader>e', '<cmd>Oil --float<cr>')
+
