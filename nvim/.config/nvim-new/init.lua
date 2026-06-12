fe57c1a67eb71027e56ca49655e8f64f29790d9c
Derek Ryms <derekryms@gmail.com>
2026-06-12 16:42:32 -0400

    Add more useful autocmds

--- nvim/.config/nvim-new/init.lua
+++ nvim/.config/nvim-new/init.lua
@@ -108,3 +108,51 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.hl.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  desc = "Restore cursor to file position in previous editing session",
+  group = vim.api.nvim_create_augroup("BufferRead", { clear = true }),
+  callback = function(args)
+    local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+    local line_count = vim.api.nvim_buf_line_count(args.buf)
+    if mark[1] > 0 and mark[1] <= line_count then
+      vim.api.nvim_win_set_cursor(0, mark)
+      -- defer centering slightly so it's applied after render
+      vim.schedule(function()
+        vim.cmd("normal! zz")
+      end)
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("VimResized", {
+  desc = "Auto resize splits when the terminal's window is resized",
+  group = vim.api.nvim_create_augroup("WindowResized", { clear = true }),
+  command = "wincmd =",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "No auto continue comments on new line",
+  group = vim.api.nvim_create_augroup("NoAutoComment", { clear = true }),
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+  end,
+})
+
+local activeCursorGroup = vim.api.nvim_create_augroup("ActiveCursorLine", { clear = true })
+
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+  desc = "Show cursorline only in active window enable",
+  group = activeCursorGroup,
+  callback = function()
+    vim.opt_local.cursorline = true
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
+  desc = "Show cursorline only in active window disable",
+  group = activeCursorGroup,
+  callback = function()
+    vim.opt_local.cursorline = false
+  end,
+})
